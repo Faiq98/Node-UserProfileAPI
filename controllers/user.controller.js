43b55d08d01a960503a6074dc1856ba20371f5ce
Faiq98 <fh.faiq@gmail.com>
2020-08-03 10:14:20 +0800

    change callback function to async/await

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,49 +1,101 @@
 const userModel = require('../models/user.model');
 
 //display all user
-exports.user_all = function (req, res) {
-    userModel.find({}, function (err, user) {
-        if (err) return next(err);
-        res.send(user);
-    });
+// exports.user_all = function (req, res) {
+//     userModel.find({}, function (err, user) {
+//         if (err) return next(err);
+//         res.send(user);
+//     });
+// }
+
+exports.user_all = async(req, res) =>{
+    const users = await userModel.find({});
+
+    try{
+        res.send(users);
+    }catch(err){
+        res.status(500).send(err);
+    }
 }
 
 //display user details by id
-exports.user_details = function(req, res){
-    userModel.findById(req.params.id, function(err, user){
-        if(err) return next (err);
+// exports.user_details = function(req, res){
+//     userModel.findById(req.params.id, function(err, user){
+//         if(err) return next (err);
+//         res.send(user);
+//     });
+// }
+
+exports.user_details = async (req, res) =>{
+    const user = await userModel.findById(req.params.id);
+
+    try{
         res.send(user);
-    });
+    }catch(err){
+        res.status(500).send(err);
+    }
 }
 
 //create user
-exports.user_create = function(req, res){
-    let user = new userModel({
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        age: req.body.age,
-        phone: req.body.phone
-    });
+// exports.user_create = function(req, res){
+//     let user = new userModel({
+//         first_name: req.body.first_name,
+//         last_name: req.body.last_name,
+//         age: req.body.age,
+//         phone: req.body.phone
+//     });
 
-    user.save(function(err){
-        if(err) return next(err);
+//     user.save(function(err){
+//         if(err) return next(err);
+//         res.send('User Successfully Create !');
+//     });
+// }
+
+exports.user_create = async(req, res)=>{
+    const user = new userModel(req.body);
+
+    try{
+        await user.save();
         res.send('User Successfully Create !');
-    });
+    }catch(err){
+        res.status(500).send(err);
+    }
 }
 
 //update user
-exports.user_update = function(req, res){
-    userModel.findByIdAndUpdate(req.params.id, {$set: req.body}, function(err, user){
-        if(err) return next(err);
+// exports.user_update = function(req, res){
+//     userModel.findByIdAndUpdate(req.params.id, {$set: req.body}, function(err, user){
+//         if(err) return next(err);
+//         res.send('Update Success');
+//     });
+// }
+
+exports.user_update = async(req, res)=>{
+    const user = await userModel.findByIdAndUpdate(req.params.id, req.body);
+
+    try{
+        await user.save();
         res.send('Update Success');
-    });
+    }catch(err){
+        res.status(500).send(err);
+    }
 }
 
 
 //delete user
-exports.user_delete = function(req, res){
-    userModel.findByIdAndRemove(req.params.id, function(err, user){
-        if(err) return next(err);
-        res.send('User has been delete');
-    });
+// exports.user_delete = function(req, res){
+//     userModel.findByIdAndRemove(req.params.id, function(err, user){
+//         if(err) return next(err);
+//         res.send('User has been delete');
+//     });
+// }
+
+exports.user_delete = async(req, res) =>{
+    try{
+        const user = await userModel.findByIdAndDelete(req.params.id);
+        if(!user) res.status(404).send('User not found !');
+        res.status(200).send('Delete Success');
+    }catch(err){
+        res.status(500).send(err);
+    }
 }
