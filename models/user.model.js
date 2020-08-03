6ffbb771890fea908bec6b1222b41f87fa0bfaf2
Faiq98 <fh.faiq@gmail.com>
2020-08-03 15:56:00 +0800

    hash password using bcrypt

--- models/user.model.js
+++ models/user.model.js
@@ -4,8 +4,8 @@ const Schema = mongoose.Schema;
 let ProfileSchema = new Schema({
     first_name: {type:String, required: true},
     last_name: {type:String, required: true},
-    age: {type:Number, required: true},
-    phone: {type:String}
+    email: {type: String, unique: true, required: true},
+    password: {type: String, required: true}
 });
 
 module.exports = mongoose.model('user', ProfileSchema);
--- routes/user.route.js
+++ routes/user.route.js
@@ -5,7 +5,7 @@ const userController = require('../controllers/user.controller');
 
 router.get('/user', userController.user_all);
 router.get('/user/:id', userController.user_details);
-router.post('/user/create', userController.user_create);
+router.post('/user/signup', userController.user_signup);
 router.put('/user/:id/update', userController.user_update);
 router.delete('/user/:id/delete', userController.user_delete);
 
