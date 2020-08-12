f90d83442d87804969da3bc3fe3440bfa388978c
Faiq98 <fh.faiq@gmail.com>
2020-08-12 14:28:20 +0800

    add cors

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const contactRoute = require('./routes/contact.route');
 const app = express();
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const cors = require('cors');
 
 let dev_db_url = 'MongoDB URL';
 const mongodb = process.env.MONGODB_URI || dev_db_url;
@@ -13,6 +14,7 @@ mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB Connection Error: '));
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(morgan('dev'));
